c9d6ed78797bc852e09f2312bd0baa0378fb418d
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-08 11:04:51 +0530

    Level 1 - Hurdle 1 - code in python

---
+++ Hurdle1.py
@@ -0,0 +1,19 @@
+#Link for game: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def goal():
+#     for i in range(6):
+#         move()
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+    
+# goal()
