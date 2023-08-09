bd3dcaf7b2fa5118bac52d5ac4e6fc09309aa2c8
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-09 22:20:41 +0530

    Hurdle 3 - Level 3 - Python code

---
+++ Hurdle3.py
@@ -0,0 +1,26 @@
+#Link for the game - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def goal():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# while not at_goal():
+#     if not front_is_clear():
+#         goal()
+#     if not wall_in_front():
+#         move()
+
+        
+#Remove all the comments before moving ahead with the command.
+# Code by Anantia Keshri        
+     
