5f6e1542d49fe891263ab1ba6ad2ac99ec763748
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-12 15:58:22 +0530

    Ceaser cipher updated game

--- Ceaser_Cipher_game.py
+++ Ceaser_Cipher_game.py
@@ -5,11 +5,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 from Cipher_art import logo
 print(logo)
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-# text = input("Type your message:\n").lower()
-# shift = int(input("Type the shift number:\n"))
-# shift = shift % 26
-
 def ceaser(plain_text, shift_amount, cipher_direction):
     start_text = ""
     if cipher_direction == "decode":
@@ -24,7 +19,6 @@ def ceaser(plain_text, shift_amount, cipher_direction):
     print(f"The {cipher_direction}d text is {start_text}.")
     
 should_continue = True
-
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
