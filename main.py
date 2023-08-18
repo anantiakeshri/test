8239effcb1b19fa12ffbabd471b054ea97e3385c
anantiakeshri <476cs18007.rp@gmail.com>
2023-08-19 00:07:46 +0530

    Quiz Game main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for item in question_data:
+    question_text = item["text"]
+    question_answer = item["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+        quiz.next_question()
+        
+        
+print(f"You've completed the quiz.\nYour final score is {quiz.score}/{quiz.question_number}")
+
